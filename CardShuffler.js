dbd6eaed6c5641935f1d441b2017344104b1bd1e
Maxwell <mlafontant@kipsu.com>
2023-02-02 13:08:40 -0600

    initial commit

---
+++ CardShuffler.js
@@ -0,0 +1,20 @@
+const shuffle = () => {
+    const cards = []
+
+    for(var i = 0; i < 52; i++) {
+        var rand = Math.floor(Math.random() * 10) % 2
+        if(cards.filter((x) => x == "Red").length != 26 && rand == 1) {    
+            cards.push("Red")
+        } else if(cards.filter((x) => x == "Black").length != 26) {
+            cards.push("Black")
+        } else {
+            cards.push("Red")
+        }
+    }
+
+    return cards
+}
+
+var deck = shuffle()
+
+console.log(deck.sort())
